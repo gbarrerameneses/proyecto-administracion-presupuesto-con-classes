1c80f4c9ff333f7d9d062d223665cc4d99cb4724
Gustavo Barrera <gustavo.8805@gmail.com>
2024-04-05 11:10:18 -0600

    creando e instanciando la classe de Presupuesto

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const gastoListado = document.querySelector('#astos ul')
 
 addEventListener();
 function addEventListener() {
-    document.addEventListener('DOMContentLoaded', preguntarpresupuesto());
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 }
 
 
@@ -22,23 +22,35 @@ function addEventListener() {
 
 // Classes
 
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto); // Number se encarga manejarlo
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
 
+class UI {
+    
+}
 
-
-
-
-
-
+// Instanciar
+const ui = new UI();
+let presupuesto; // variable global de presupuesto sin valor
 
 
 // Funciones
 
-function preguntarpresupuesto() {
+function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
-    console.log(parseFloat(presupuestoUsuario));
+    // console.log(parseFloat(presupuestoUsuario));
 
     // Validación para no saltar la alerta
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0 ) {
         window.location.reload();
     }
+
+    // Instanciando
+    presupuesto = new Presupuesto(presupuestoUsuario); // una vez que tengamos un presupuesto asignamos ese valor
+    console.log(presupuesto);
 }
